48376292860627e73b08b3171d1195aeb908fdce
Brent Klein <brentcklein@gmail.com>
2017-04-28 15:10:27 -0400

    Finished version 1.0.0

--- background.js
+++ background.js
@@ -1,7 +1,27 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('smithing words');
-  chrome.tabs.executeScript(null, {
+  chrome.tabs.executeScript({
     file: "smith.js"
   });
 });
+
+chrome.runtime.onMessage.addListener(
+	function (request, sender, sendResponse) {
+		if (request.string) {
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', 'http://fuzzylogicinc.net/boggle/Solver.svc?BoardID=' + request.string + '&Length=3');
+			xhr.onreadystatechange = function () {
+				var result = {};
+				if (xhr.readyState === XMLHttpRequest.DONE) {
+					if (xhr.status === 200) {
+						result = {success: true, data: JSON.parse(xhr.responseText)};
+					} else {
+						result = {error: true, data: JSON.parse(xhr.responseText)}
+					}
+					sendResponse(result);
+				}
+			};
+			xhr.send();
+		}
+		return true;
+	}
+);
--- smith.js
+++ smith.js
@@ -1,30 +1,53 @@
-var board = document.getElementById('b4x4').getElementsByTagName('TABLE')[0];
+var iframe    = document.getElementsByName('main')[0], 
+                /* or other kind of reference to the iframe DOM node */
+
+    iframeDoc = ((!!iframe['contentDocument']) 
+              ? iframe.contentDocument 
+              : iframe.contentWindow.document),
+
+    board     = iframeDoc.getElementById('b4x4'),
+                /* this is a reference to cell inside the iframe */
+    input	  = iframeDoc.getElementById('input');
+    button	  = iframeDoc.getElementById('ext-gen207');
+
 
 var letters = [];
 var rows = board.getElementsByTagName('TR');
-
-for (var i = rows.length - 1; i >= 0; i--) {
+var l;
+for (var i = 0; i <= rows.length - 1; i++) {
 	var lets = rows[i].getElementsByTagName('TD');
-	for (var k = lets.length - 1; k >= 0; k--) {
-		letters.push(lets[k].innerHTML);
+	for (var k = 0; k <= lets.length - 1; k++) {
+		if (lets[k].innerHTML == 'Qu') {
+			l = 'Q';
+		} else {
+			l = lets[k].innerHTML;
+		}
+		letters.push(l);
 	}
 }
 
 var boardString = letters.join('');
 
-var request = XMLHttpRequest();
-request.onreadystatechange = enterAnswers;
-
-request.open('GET', 'http://fuzzylogicinc.net/boggle/Solver.svc?BoardID=' + boardString + '&Length=3');
-request.SEND()
-
-function enterAnswers() {
-	if (request.readyState === XMLHttpRequest.DONE) {
-		if (request.status === 200) {
-			alert('logging');
-			console.log(request.responseText);
-		} else {
-			alert('there was a problem');
+console.log('fetching solutions for board: ' + boardString);
+chrome.runtime.sendMessage({string: boardString}, function (response) {
+	if (response.success) {
+		var word;
+		for (var i = response.data.Solutions.length - 1; i >= response.data.Solutions.length; i--) {
+			word = response.data.Solutions[i].Word;
+			console.log('setting input to ' + word);
+			input.value = word;
+			console.log('submitting form');
+			eventFire(button, 'click');
 		}
 	}
+});
+
+function eventFire(el, etype){
+  if (el.fireEvent) {
+    el.fireEvent('on' + etype);
+  } else {
+    var evObj = document.createEvent('Events');
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
+  }
 }
